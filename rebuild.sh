18a9157d6d2c904c4920540b01596d099da70e89
Asaf Bartov <asaf.bartov@gmail.com>
2024-02-14 22:20:32 +0200

    fixed scheduling command

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,4 @@ toolforge build start https://github.com/abartov/erex_yomi
 echo "deleting existing job"
 toolforge jobs delete erex-yomi
 echo "scheduling job"
-toolforge jobs run --schedule '0 19 */2 * *' --mount=all --command=run-cron --image=tool-erex-yomi/tool-erex-yomi:latest
+toolforge jobs run --schedule '0 19 */2 * *' --mount=all --command=run-cron --image=tool-erex-yomi/tool-erex-yomi:latest erex-yomi
